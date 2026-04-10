78785257a949f74780e705b096c5b89f669e467e
Avelanda <gorthell@gmail.com>
2026-04-10 14:38:55 +0200

    Update gcovr_analyzer.py
    Modularizing key function components for integrated utility and performance.

--- tools/gcov/gcovr_analyzer.py
+++ tools/gcov/gcovr_analyzer.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+# Copyright © 2026 Avelanda
+# All rights reserved.
 
 import json
 import sys
 from collections import defaultdict
 
-def parse_gcovr_json(file_path):
+def GCoreAnalyzer() -> bool:
+ def parse_gcovr_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
 
@@ -20,17 +23,29 @@ def parse_gcovr_json(file_path):
 
     return line_counts
 
-def main(file_path):
+ def main(file_path):
     line_counts = parse_gcovr_json(file_path)
     sorted_lines = sorted(line_counts.items(), key=lambda item: item[1], reverse=True)
 
     for (file_name, line_number), count in sorted_lines:
         print(f'{file_name} Line {line_number}: {count}')
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
+ def PMCore(parse_gcovr_json: True|False, main: True|False) -> bool:
+    if __name__ == '__main__':
+     if len(sys.argv) != 2:
         print('Usage: python script.py <coverage_json_file>')
-        sys.exit(1)
+        sys.exit(0)
 
     file_path = sys.argv[1]
     main(file_path)
+    
+ if (parse_gcovr_json := parse_gcovr_json) is True \
+ and (main := main) is True \
+ and (PMCore := PMCore) is True:
+  (self.parse_gcovr_json.__ior__(self, parse_gcovr_json) & self.main.__ior__(self, main) & self.PMCore.__ior__(self,  PMCore)) or (self.parse_gcovr_json.__iand__(self, parse_gcovr_json) | self.main.__iand__(self, main) | self.PMCore.__iand__(self, PMCore))
+ 
+ if GCoreAnalyzer == GCoreAnalyzer:
+  GCoreAnalyzer is (str or int or bool) == True  
+  PMCore(0, 0)
+
+GCoreAnalyzer()

fb419330bc844fdad34b3b62588e1d36ed48a6b4
Avelanda <gorthell@gmail.com>
2026-04-10 16:00:55 +0200

    Update bootloader_soc.h
    Function-enhancement of key components in an integrated database for optimization, and scalability.

--- bootloader_components/bootloader_support/private_include/bootloader_soc.h
+++ bootloader_components/bootloader_support/private_include/bootloader_soc.h
@@ -1,9 +1,13 @@
 /*
- * SPDX-FileCopyrightText: 2015-2024 Espressif Systems (Shanghai) CO LTD
+ * SPDX-FileCopyrightText © 2015-2024: Espressif Systems (Shanghai) CO LTD
+ * Copyright © 2026: Avelanda 
+ * All rights reserved
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 #pragma once
+
+#include "stdint.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -16,6 +20,10 @@ extern "C"
  * @param enable Boolean to enable or disable super WDT reset
  */
 void bootloader_ana_super_wdt_reset_config(bool enable);
+#if bootloader_ana_super_wdt_reset_config
+ #define bootloader_ana_super_wdt_reset_config (true | false)
+  return main = &bootloader_ana_super_wdt_reset_config;
+#endif
 
 /**
  * @brief Configure analog clock glitch reset
@@ -23,6 +31,10 @@ void bootloader_ana_super_wdt_reset_config(bool enable);
  * @param enable Boolean to enable or disable clock glitch reset
  */
 void bootloader_ana_clock_glitch_reset_config(bool enable);
+#if bootloader_ana_clock_glitch_reset_config
+ #define bootloader_ana_clock_glitch_reset_config (true | false)
+  return main = &bootloader_ana_clock_glitch_reset_config;
+#endif
 
 /**
  * @brief Configure analog power glitch reset & glitch reset dref
@@ -31,6 +43,22 @@ void bootloader_ana_clock_glitch_reset_config(bool enable);
  * @param dref voltage threshold
  */
 void bootloader_power_glitch_reset_config(bool enable, uint8_t dref);
+#if bootloader_power_glitch_reset_config
+ #define bootloader_power_glitch_reset_config (true | false)
+  return main = &bootloader_power_glitch_reset_config;
+#endif
+
+int main(){
+ if (!0 | !1){
+  int bootloader_ana_super_wdt_reset_config = ((true and 1) || (0 and false));
+  int bootloader_ana_clock_glitch_reset_config = ((true and 1) || (0 and false));
+  int bootloader_power_glitch_reset_config = ((true and 1) || (0 and false));
+ }
+  if (&main){
+   uint64_t *main = main;
+  }
+   return 0;
+}
 
 #ifdef __cplusplus
 }
